bcfecc3ba2cd11852f91f5e1c0387dc86719388d
vit <firstvirpil@gmail.com>
2022-12-17 23:24:24 +0700

    six

--- src/baseclasses/response6.py
+++ src/baseclasses/response6.py
@@ -7,7 +7,7 @@ class Response:
 
     def __init__(self, response):
         self.response = response
-        self.response_json_data = response.json().get('data')
+        self.response_json = response.json().get('data')
         self.response_status = response.status_code
 
     def validate(self, schema):
