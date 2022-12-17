c8516af04ec045eb317b05a9a5c370747a7199f9
vit <firstvirpil@gmail.com>
2022-12-17 17:31:45 +0700

    second

--- tests/test_something.py
+++ tests/test_something.py
@@ -3,12 +3,8 @@ import requests
 from configuration import SERVICE_URL
 
 from src.baseclasses.response import Response
+from src.enums.global_enums import GlobalErrorMessages
 from src.schemas.post import POST_SCHEMA
-
-
-def test_equal():
-    assert 1 == 1
-
 
 
 def test_getting_post():
@@ -17,9 +13,9 @@ def test_getting_post():
     response.assert_status_code(200).validate(POST_SCHEMA)
 
 
-def test_getting_post1():
+def test_count_posts():
     r = requests.get(url=SERVICE_URL)
-    response = Response(r)
-    response.assert_status_code(500).validate(POST_SCHEMA)
+    received_posts = r.json()
+    assert len(received_posts) == 3, GlobalErrorMessages.WRONG_ELEMENT_COUNT.value
 
 # [{'id': 1, 'title': 'Post 1'}, {'id': 2, 'title': 'Post 2'}, {'id': 3, 'title': 'Post 3'}]
