22806b094ffd5d1846d23e1e1e4469b52ec8f5ae
vit <firstvirpil@gmail.com>
2022-12-17 23:22:56 +0700

    six

--- tests/test_with_pydantic.py
+++ tests/test_with_pydantic.py
@@ -5,7 +5,7 @@ from src.baseclasses.response import Response
 from src.pydantic_schemas.pydantic_post import Post
 
 
-def test_status_code():
+def test_status_code_pydantic():
     r = requests.get(url=SERVICE_URL)
     response = Response(r)
     response.assert_status_code(200).validate(Post)
